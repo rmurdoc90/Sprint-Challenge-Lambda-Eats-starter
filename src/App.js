3f863c94c7b8d0f3725b9ec467a414adece49dcf
Robert Murdoch <rmurdoc90@gmail.com>
2020-03-27 09:17:35 -0700

    adding form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React from "react"
 import { Route, Link, Switch } from 'react-router-dom'
+import OrderPizza from "./components/OrderPizza"
+import Home from './components/Home'
+
+
 
 const App = () => {
   return (
@@ -8,13 +12,13 @@ const App = () => {
       <h1 className='Logo'>Lambda Eats</h1>
       <nav>
           <Link to="/"> Home </Link>
-          <Link to="/order"> Order Pizza </Link>
+          <Link to="/pizza"> Order Pizza </Link>
       </nav>
       <Switch>
         <Route exact path='/'>
           <Home/>
         </Route>
-        <Route path='/order'>
+        <Route path='/pizza'>
           <OrderPizza/>
         </Route>
 
---
+++ src/components/OrderPizza.js
@@ -0,0 +1,110 @@
+import React, { useState} from "react";
+import axios from "axios";
+
+
+export default function OrderPizza(){
+    
+    const [post, setPost] = useState([]);
+    
+    const [formState, setFormState] = useState({
+        name:'',
+        size:'',
+        ham:'',
+        pineapple:'',
+        jalapenos:'',
+        sauce:'',
+
+    })
+
+    const formSubmit = e =>{
+        e.preventDefault();
+        axios
+        .post("https://reqres.in/api/users", formState)
+        .then(res => {
+            setPost(res.data);
+            console.log("success", post);
+            setFormState({
+            name: '',
+            size:'',
+            ham:'',
+            pineapple:'',
+            jalapenos:'',
+            sauce:'',
+            })
+        })
+        .catch(err => console.log(err.response));
+    }
+
+    const inputChange = e => {
+        e.persist();
+        const newFormData = {
+          ...formState,
+          [e.target.name]:
+            e.target.type === "checkbox" ? e.target.checked : e.target.value
+        };
+        setFormState(newFormData);
+      };
+
+    return (
+        <form onSubmit = {formSubmit}>
+            <label htmlFor='name'>
+                Name 
+                <input
+                type='text'
+                name='name'
+                value={formState.name}
+                onChange={inputChange}
+                />
+            </label>
+
+            <label htmlFor='size'>
+                What Size 
+                <select id='size' name='size' onChange={inputChange}>
+                    <option value='Small'>Small</option>
+                    <option value='Medium'>Medium</option>
+                    <option value='Large'>Large</option>
+                    <option value='XL'>XL</option>
+                </select>
+            </label>
+            <h3>Toppings</h3>
+            <label htmlFor='ham' className='ham'>
+                <input
+                    type='checkbox'
+                    name="ham"
+                    checked={formState.ham}
+                    onChange={inputChange}
+                    />
+                Ham
+                </label>
+                <label htmlFor='pineapple' className='pineapple'>
+                <input
+                    type='checkbox'
+                    name="pineapple"
+                    checked={formState.pineapple}
+                    onChange={inputChange}
+                    />
+                Pineapple
+                </label>
+                <label htmlFor='jalapenos' className='jalapenos'>
+                <input
+                    type='checkbox'
+                    name="jalapenos"
+                    checked={formState.jalapenos}
+                    onChange={inputChange}
+                    />
+                Jalapenos
+                </label>
+                <label htmlFor='sauce' className='sauce'>
+                <input
+                    type='checkbox'
+                    name="sauce"
+                    checked={formState.sauce}
+                    onChange={inputChange}
+                    />
+                Hot Sauce
+                </label>
+                <pre>{JSON.stringify(post, null, 2)}</pre>
+                <button >Submit</button>
+        </form>
+    )
+}
